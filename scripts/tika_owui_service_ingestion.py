03df09680cbcedecd498bd29546c202acc33fb2f
Daniel Andreasen <akda@aarhus.dk>
2025-06-26 14:42:35 +0200

    Write tika ingestion test script

--- scripts/tika_owui_service_ingestion.py
+++ scripts/tika_owui_service_ingestion.py
@@ -1,6 +1,37 @@
+import os
 import requests
+import argparse
+from urllib.parse import urlparse, quote
+from typing import List
+from dotenv import load_dotenv
+from langchain_core.documents import Document
 from owui_doc_ingestion.doc_loaders.tika import TikaLoader
 
+load_dotenv()
+
+TIKA_SERVER_URL = os.getenv('TIKA_SERVER_URL')
+TIKA_SERVER_USER = os.getenv('TIKA_SERVER_USER')
+TIKA_SERVER_PWD = os.getenv('TIKA_SERVER_PWD')
+
+def set_url_username_password(url, username, password):
+  _username = quote(username)
+  _password = quote(password)
+  _url = urlparse(url)
+  _netloc = _url.netloc.split('@')[-1]
+  _url = _url._replace(netloc=f'{_username}:{_password}@{_netloc}')
+  return _url.geturl()
 
 def main():
-    loader = TikaLoader(
+    parser = argparse.ArgumentParser(description="Process a list of file paths.")
+    parser.add_argument('file_paths', nargs='+', help='List of paths to files')
+
+    args = parser.parse_args()
+    url = set_url_username_password(TIKA_SERVER_URL, TIKA_SERVER_USER, TIKA_SERVER_PWD)
+    docs: List[Document] = []
+    for file_path in args.file_paths:
+        loader = TikaLoader(url, file_path, extract_images=None)
+        docs.append(loader.load())
+
+
+if __name__ == "__main__":
+    main()
