a73038b712dcae2b93701347f86114580dc0fd82
Daniel Andreasen <akda@aarhus.dk>
2025-06-27 08:45:50 +0200

    Minor corrections to tika ingestion script

--- scripts/tika_owui_service_ingestion.py
+++ scripts/tika_owui_service_ingestion.py
@@ -1,5 +1,4 @@
 import os
-import requests
 import argparse
 from urllib.parse import urlparse, quote
 from typing import List
@@ -12,6 +11,8 @@ load_dotenv()
 TIKA_SERVER_URL = os.getenv('TIKA_SERVER_URL')
 TIKA_SERVER_USER = os.getenv('TIKA_SERVER_USER')
 TIKA_SERVER_PWD = os.getenv('TIKA_SERVER_PWD')
+
+PDF_EXTRACT_IMAGES = os.getenv('PDF_EXTRACT_IMAGES',False).lower() in ['true', '1', 't']
 
 def set_url_username_password(url, username, password):
   _username = quote(username)
@@ -29,8 +30,8 @@ def main():
     url = set_url_username_password(TIKA_SERVER_URL, TIKA_SERVER_USER, TIKA_SERVER_PWD)
     docs: List[Document] = []
     for file_path in args.file_paths:
-        loader = TikaLoader(url, file_path, extract_images=None)
-        docs.append(loader.load())
+        loader = TikaLoader(url, file_path, extract_images= PDF_EXTRACT_IMAGES)
+        docs = docs + loader.load()
 
 
 if __name__ == "__main__":
