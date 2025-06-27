74cd87d437e1742f7f3b2456d437ab29b07249ef
Daniel Andreasen <akda@aarhus.dk>
2025-06-27 10:40:17 +0200

    Refactor script with urls utils

--- scripts/tika_owui_service_ingestion.py
+++ scripts/tika_owui_service_ingestion.py
@@ -1,10 +1,10 @@
 import os
 import argparse
-from urllib.parse import urlparse, quote
 from typing import List
 from dotenv import load_dotenv
 from langchain_core.documents import Document
 from owui_doc_ingestion.doc_loaders.tika import TikaLoader
+from owui_doc_ingestion.utils.urls import set_url_username_password
 
 load_dotenv()
 
@@ -13,14 +13,6 @@ TIKA_SERVER_USER = os.getenv('TIKA_SERVER_USER')
 TIKA_SERVER_PWD = os.getenv('TIKA_SERVER_PWD')
 
 PDF_EXTRACT_IMAGES = os.getenv('PDF_EXTRACT_IMAGES',False).lower() in ['true', '1', 't']
-
-def set_url_username_password(url, username, password):
-  _username = quote(username)
-  _password = quote(password)
-  _url = urlparse(url)
-  _netloc = _url.netloc.split('@')[-1]
-  _url = _url._replace(netloc=f'{_username}:{_password}@{_netloc}')
-  return _url.geturl()
 
 def main():
     parser = argparse.ArgumentParser(description="Process a list of file paths.")
---
+++ src/owui_doc_ingestion/utils/urls.py
@@ -0,0 +1,24 @@
+from urllib.parse import urlparse, quote
+
+def set_url_username_password(url, username, password):
+    """
+    Sets the URL, username, and password for authentication or configuration purposes.
+
+    This function allows you to assign the provided URL, username, and password. It can be used
+    in contexts where credentials and endpoint configuration are required, ensuring proper
+    assignment for further operations.
+
+    :param url: The target URL to configure.
+    :type url: str
+    :param username: The username for authentication.
+    :type username: str
+    :param password: The password for authentication.
+    :type password: str
+    :return: None
+    """
+    _username = quote(username)
+    _password = quote(password)
+    _url = urlparse(url)
+    _netloc = _url.netloc.split('@')[-1]
+    _url = _url._replace(netloc=f'{_username}:{_password}@{_netloc}')
+    return _url.geturl()
