c7d285ee8441a578a32b82f1effe8c5d9047262a
Daniel Andreasen <akda@aarhus.dk>
2025-06-30 08:50:51 +0200

    Record processing metric from docling script as well

--- scripts/docling_owui_service_ingestion.py
+++ scripts/docling_owui_service_ingestion.py
@@ -1,9 +1,11 @@
 import os
 import argparse
+import time
 from dotenv import load_dotenv
 from owui_doc_ingestion.doc_loaders.docling import DoclingLoader
-from owui_doc_ingestion.utils.urls import (set_url_username_password)
+from owui_doc_ingestion.utils.urls import set_url_username_password
 from owui_doc_ingestion.utils.doc_io import save_docs_to_md
+from owui_doc_ingestion.processing_metrics.db import MetricsDatabaseConnection
 
 load_dotenv()
 
@@ -27,10 +29,15 @@ def main():
 
     args = parser.parse_args()
     url = set_url_username_password(DOCLING_SERVER_URL, DOCLING_SERVER_USER, DOCLING_SERVER_PWD)
-    for file_path in args.file_paths:
-        loader = DoclingLoader(url, file_path, params=DOCLING_PARAMS)
-        docs = loader.load()
-        save_docs_to_md(docs, file_path, args.out_folder)
+    with MetricsDatabaseConnection() as conn:
+        for file_path in args.file_paths:
+            loader = DoclingLoader(url, file_path, params=DOCLING_PARAMS)
+            start_time = time.time()
+            docs = loader.load()
+            processing_time = time.time() - start_time
+            conn.save_metrics('docling', file_path, processing_time)
+
+            save_docs_to_md(docs, file_path, args.out_folder)
 
 
 if __name__ == "__main__":
